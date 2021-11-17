1faa03eb91fe6faa917745ce8a00fe5605abadbc
chen-chili <chen.chili7@gmail.com>
2021-11-16 21:19:30 -0500

    third test

--- test_portfolio.py
+++ test_portfolio.py
@@ -10,3 +10,7 @@ def test_buy_two_stocks():
     p.buy("IBM", 100, 176.48)
     p.buy("HPQ", 100, 36.15)
     assert p.cost() == 21263.0
+
+def test_empty_portfolio():
+    p = Portfolio()
+    assert p.cost() == 0.0
