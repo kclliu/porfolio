629e65bccd3cd3b3f33df7bf18849f1e4e07e122
chen-chili <chen.chili7@gmail.com>
2021-11-16 20:54:08 -0500

    correct

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,11 +1,12 @@
 from portfolio import Portfolio
 
-p = Portfolio()
-print(f"Empty portfolio cost: {p.cost()}, should be 0.0")
-assert p.cost() == 0.0
-p.buy("IBM", 100, 176.48)
-print(f"With 100 IBM @ 176.48: {p.cost()}, should be 17648.0")
-assert p.cost() == 17648.0
-p.buy("HPQ", 100, 36.15)
-print(f"With 100 HPQ @ 36.15: {p.cost()}, should be 21263.0")
-assert p.cost() == 21263.0
+def test_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    assert p.cost() == 17648.0
+
+def test_buy_two_stocks():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    p.buy("HPQ", 100, 36.15)
+    assert p.cost() == 21263.0
